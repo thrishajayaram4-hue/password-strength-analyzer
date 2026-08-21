dfc5ccd26c62b97f6f9cf3d61659229e1c75756c
thrishajayaram4-hue <thrishajayaram4@gmail.com>
2026-08-21 12:53:16 +0530

    Update password_analyzer.py

--- password_analyzer.py
+++ password_analyzer.py
@@ -9,6 +9,8 @@ has_lowercase = any(char.islower() for char in password)
 print("Contains lowercase:",has_lowercase)
 has_number = any(char.isdigit() for char in password)
 print("Contains number:",has_number)
+has_special = any(not char.isalnum() for char in password)
+print("Contains special character:",has_special)
 
 
 
